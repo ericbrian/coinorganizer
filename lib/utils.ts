73781127396f6aa3d8c297fd21e1ba1d753ac626
Eric Coffman <ericbrian@gmail.com>
2023-06-30 19:38:46 -0400

    Add Engraver last name sort.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
+import {
+  engraver as engraverDb,
+} from "@prisma/client";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function engraversSort(a: engraverDb, b: engraverDb) {
+  const aParts = a.name.split(" ");
+  const bParts = b.name.split(" ");
+  return `${aParts.at(-1)} ${aParts.at(0)}`.localeCompare(
+    `${bParts.at(-1)} ${bParts.at(0)}`
+  );
 }
