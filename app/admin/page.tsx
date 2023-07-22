ceeadda443bd4d92f28af6e2a9dda86421ee5b7b
Eric Coffman <ericbrian@gmail.com>
2023-07-22 09:48:13 -0400

    Change list styling.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, Button } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
 
@@ -15,21 +15,55 @@ export default function page() {
                 </Typography>
 
                 <Typography variant="h6">
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/country/view'}>Country</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/currency/view'}>Currency</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/engraver/view'}>Engraver/Designer</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/mint/view'}>Mint</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/period/view'}>Period</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/ruler/view'}>Ruler</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
-                    <Link href={'/admin/shape/view'}>Shape</Link>{' '}
-                    &nbsp;&middot;&nbsp;{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/country/view"
+                    >
+                        Country
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/currency/view"
+                    >
+                        Currency
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/engraver/view"
+                    >
+                        Engraver/Designer
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/mint/view"
+                    >
+                        Mint
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/period/view"
+                    >
+                        Period
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/ruler/view"
+                    >
+                        Ruler
+                    </Button>{' '}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/admin/shape/view"
+                    >
+                        Shape
+                    </Button>{' '}
                 </Typography>
             </Box>
         </Container>
