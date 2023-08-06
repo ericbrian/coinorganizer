1262aa9676ed6a91f8d96d82d707b79b79034ddd
Eric Coffman <ericbrian@gmail.com>
2023-08-06 18:14:34 -0400

    Rename.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,101 +2,53 @@ import { Container, Box, Typography, Button } from '@mui/material';
 import React from 'react';
 
 export default function page() {
-    return (
-        <Container>
-            <Box>
-                <Typography
-                    variant="h4"
-                    style={{ fontWeight: 'bold' }}
-                    gutterBottom
-                >
-                    Data Management
-                </Typography>
+  return (
+    <Container>
+      <Box>
+        <Typography variant="h4" style={{ fontWeight: 'bold' }} gutterBottom>
+          Data Management
+        </Typography>
 
-                <Typography variant="h6">
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/coin/view"
-                    >
-                        Coin
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/country/view"
-                    >
-                        Country
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/currency/view"
-                    >
-                        Currency
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/engraver/view"
-                    >
-                        Engraver/Designer
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/mint/view"
-                    >
-                        Mint
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/period/view"
-                    >
-                        Period
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/ruler/view"
-                    >
-                        Ruler
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/shape/view"
-                    >
-                        Shape
-                    </Button>{' '}
-                </Typography>
-            </Box>
-            <Box sx={{ mt: 5 }}>
-                <Typography
-                    variant="h4"
-                    style={{ fontWeight: 'bold' }}
-                    gutterBottom
-                >
-                    Other
-                </Typography>
-                <Typography variant="h6">
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/user/dashboard/add-coin"
-                    >
-                        Add Coin to your Collection
-                    </Button>{' '}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        href="/admin/coin/add-images-to-coins"
-                    >
-                        Add Images to Coins
-                    </Button>{' '}
-                </Typography>{' '}
-            </Box>
-        </Container>
-    );
+        <Typography variant="h6">
+          <Button variant="contained" color="primary" href="/admin/coin/view">
+            Coin
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/country/view">
+            Country
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/currency/view">
+            Currency
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/engraver/view">
+            Engraver/Designer
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/mint/view">
+            Mint
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/period/view">
+            Period
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/ruler/view">
+            Ruler
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/shape/view">
+            Shape
+          </Button>{' '}
+        </Typography>
+      </Box>
+      <Box sx={{ mt: 5 }}>
+        <Typography variant="h4" style={{ fontWeight: 'bold' }} gutterBottom>
+          Other
+        </Typography>
+        <Typography variant="h6">
+          <Button variant="contained" color="primary" href="/user/dashboard/add-coin">
+            Add Coin to your Collection
+          </Button>{' '}
+          <Button variant="contained" color="primary" href="/admin/coin/add-images">
+            Add Images to Coins
+          </Button>{' '}
+        </Typography>{' '}
+      </Box>
+    </Container>
+  );
 }
