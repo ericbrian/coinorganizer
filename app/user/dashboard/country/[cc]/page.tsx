1e999d7e1cb69848297950302ec75e143d933baf
Eric Coffman <ericbrian@gmail.com>
2023-08-10 19:49:15 -0400

    Add ids.

--- app/user/dashboard/country/[cc]/page.tsx
+++ app/user/dashboard/country/[cc]/page.tsx
@@ -9,61 +9,69 @@ import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export default function page() {
-    const params = useParams();
-    const countryCode = (params.cc as string) || null;
+  const params = useParams();
+  const countryCode = (params.cc as string) || null;
 
-    const [countryName, setCountryName] = useState(params.cc as string);
-    const [collectionItems, setCollectionItems] = useState<CollectionType[]>([]);
+  const [countryName, setCountryName] = useState(params.cc as string);
+  const [collectionItems, setCollectionItems] = useState<CollectionType[]>([]);
 
-    useEffect(() => {
-        getUserCollectionCountries(countryCode).then((data: CollectionType[]) => {
-            if (data) {
-                setCountryName(data[0].coin.country.short_name);
-            }
-            setCollectionItems(data);
-        });
-    }, []);
+  useEffect(() => {
+    getUserCollectionCountries(countryCode).then((data: CollectionType[]) => {
+      if (data) {
+        setCountryName(data[0].coin.country.short_name);
+      }
+      setCollectionItems(data);
+    });
+  }, []);
 
-    return (
-        <Container>
-            <Box>
-                <Typography variant="body2" gutterBottom>
-                    <Link href="../">&lt;&lt;&lt; Go Back</Link>
-                </Typography>
-                <Typography variant="h6">Your Coins by Country</Typography>
-                <Typography variant="h4" gutterBottom>
-                    {countryName}
-                </Typography>
-                {collectionItems.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <Card
-                                sx={{
-                                    display: 'flex',
-                                    marginBottom: '15px',
-                                    padding: '5px',
-                                    backgroundColor: '#008000',
-                                    color: '#ffffff',
-                                }}
-                            >
-                                <Grid container spacing={2}>
-                                    <Grid item xs={3}>
-                                        Paid: {item.currency.name} {item.currency.short_name}{' '}
-                                        {item.paid_amount ? (+item.paid_amount).toFixed(2) : '0.00'}
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        Purchased On: {moment(item.sourced_when).format('MMM D, yyyy')}
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        Purchased From: {item.sourced_from}
-                                    </Grid>
-                                </Grid>
-                            </Card>
-                            <HomePageCoinDetail coin={item.coin} />
-                        </div>
-                    );
-                })}
-            </Box>
-        </Container>
-    );
+  return (
+    <Container>
+      <Box>
+        <Typography variant="body2" gutterBottom>
+          <Link href="../">&lt;&lt;&lt; Go Back</Link>
+        </Typography>
+        <Typography variant="h6">Your Coins by Country</Typography>
+        <Typography variant="h4" gutterBottom>
+          {countryName}
+        </Typography>
+        {collectionItems.map((item) => {
+          return (
+            <div key={item.id}>
+              <Card
+                sx={{
+                  display: 'flex',
+                  marginBottom: '15px',
+                  padding: '5px',
+                  backgroundColor: '#008000',
+                  color: '#ffffff',
+                }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={1}>
+                    Y: {item.year}
+                  </Grid>
+                  <Grid item xs={2}>
+                    Paid: {item.currency.name} {item.currency.short_name}{' '}
+                    {item.paid_amount ? (+item.paid_amount).toFixed(2) : '0.00'}
+                  </Grid>
+                  <Grid item xs={3}>
+                    Purchased: {moment(item.sourced_when).format('MMM D, yyyy')}
+                  </Grid>
+                  <Grid item xs={4}>
+                    Purchased From: {item.sourced_from}
+                  </Grid>
+                  <Grid item xs={2}>
+                    <small>
+                      Coin# {item.coin.id} / Col# {item.id}
+                    </small>
+                  </Grid>
+                </Grid>
+              </Card>
+              <HomePageCoinDetail coin={item.coin} />
+            </div>
+          );
+        })}
+      </Box>
+    </Container>
+  );
 }
