c922049a89ff54191b7e6063385e32d8b4d4d3d6
Eric Coffman <ericbrian@gmail.com>
2023-08-17 18:13:36 -0400

    Don't assume url hasn't been manipulated.

--- app/user/dashboard/country/[cc]/page.tsx
+++ app/user/dashboard/country/[cc]/page.tsx
@@ -12,15 +12,18 @@ export default function page() {
   const params = useParams();
   const countryCode = (params.cc as string) || null;
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState('No coins found!');
   const [countryName, setCountryName] = useState(params.cc as string);
   const [collectionItems, setCollectionItems] = useState<CollectionType[]>([]);
 
   useEffect(() => {
     getUserCollectionCountries(countryCode).then((data: CollectionType[]) => {
-      if (data) {
+      setIsLoading(false);
+      if (data?.length > 0) {
         setCountryName(data[0].coin.country.short_name);
-      }
-      setCollectionItems(data);
+        setCollectionItems(data);
+      } else if ('message' in data) setMessage(data.message as string);
     });
   }, []);
 
@@ -34,43 +37,47 @@ export default function page() {
         <Typography variant="h4" gutterBottom>
           {countryName}
         </Typography>
-        {collectionItems.map((item) => {
-          return (
-            <div key={item.id}>
-              <Card
-                sx={{
-                  display: 'flex',
-                  marginBottom: '15px',
-                  padding: '5px',
-                  backgroundColor: '#008000',
-                  color: '#ffffff',
-                }}
-              >
-                <Grid container spacing={2}>
-                  <Grid item xs={1}>
-                    Y: {item.year}
+        {isLoading && <div>Loading...</div>}
+        {!isLoading && collectionItems.length === 0 && <div>{message}</div>}
+        {!isLoading &&
+          collectionItems.length > 0 &&
+          collectionItems.map((item) => {
+            return (
+              <div key={item.id}>
+                <Card
+                  sx={{
+                    display: 'flex',
+                    marginBottom: '15px',
+                    padding: '5px',
+                    backgroundColor: '#008000',
+                    color: '#ffffff',
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={1}>
+                      Y: {item.year}
+                    </Grid>
+                    <Grid item xs={2}>
+                      Paid: {item.currency.name} {item.currency.short_name}{' '}
+                      {item.paid_amount ? (+item.paid_amount).toFixed(2) : '0.00'}
+                    </Grid>
+                    <Grid item xs={3}>
+                      Purchased: {moment(item.sourced_when).format('MMM D, yyyy')}
+                    </Grid>
+                    <Grid item xs={4}>
+                      Purchased From: {item.sourced_from}
+                    </Grid>
+                    <Grid item xs={2}>
+                      <small>
+                        Coin# {item.coin.id} / Col# {item.id}
+                      </small>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={2}>
-                    Paid: {item.currency.name} {item.currency.short_name}{' '}
-                    {item.paid_amount ? (+item.paid_amount).toFixed(2) : '0.00'}
-                  </Grid>
-                  <Grid item xs={3}>
-                    Purchased: {moment(item.sourced_when).format('MMM D, yyyy')}
-                  </Grid>
-                  <Grid item xs={4}>
-                    Purchased From: {item.sourced_from}
-                  </Grid>
-                  <Grid item xs={2}>
-                    <small>
-                      Coin# {item.coin.id} / Col# {item.id}
-                    </small>
-                  </Grid>
-                </Grid>
-              </Card>
-              <HomePageCoinDetail coin={item.coin} />
-            </div>
-          );
-        })}
+                </Card>
+                <HomePageCoinDetail coin={item.coin} />
+              </div>
+            );
+          })}
       </Box>
     </Container>
   );
