897dec086209034970bf6001774f5081d62aa2ae
Eric Coffman <ericbrian@gmail.com>
2023-08-18 19:25:09 -0400

    Fix regression

--- app/admin/country/create/page.tsx
+++ app/admin/country/create/page.tsx
@@ -46,6 +46,8 @@ export default function AddCountry() {
           .finally(() => {
             setIsLoading(false);
           });
+      } else {
+        setIsLoading(false);
       }
     });
   }, []);
