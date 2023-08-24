a4beaffcb43f3be514b113af088e9a751e095745
Eric Coffman <ericbrian@gmail.com>
2023-08-24 19:17:10 -0400

    Add account page.

---
+++ app/user/account/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Box, Container } from '@mui/material';
+
+export default function Account() {
+  return (
+    <Container>
+      <Box>
+        <h1>Account</h1>
+      </Box>
+    </Container>
+  );
+}
