14bed4677c9b8d4b1507b654197b126cea65e57f
Eric Coffman <ericbrian@gmail.com>
2023-12-01 19:28:22 -0500

    Updated packages.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true
-    },
-    images: {
-        domains: ['github.com', 'lh3.googleusercontent.com', 'localhost', 'coin-organizer.b-cdn.net']
-    }
+  experimental: {
+  },
+  images: {
+    domains: ['github.com', 'lh3.googleusercontent.com', 'localhost', 'coin-organizer.b-cdn.net']
+  }
 }
 
 module.exports = nextConfig
